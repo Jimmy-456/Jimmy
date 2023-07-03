b094e588947342147a4b31183c5a61c5f7788722
Jimmy-456 <138448287+Jimmy-456@users.noreply.github.com>
2023-07-03 15:04:05 +0600

    Add files via upload

---
+++ Lists-Tuples-and Dictionaries.py
@@ -0,0 +1,24 @@
+myfruitList = ["apple", "banana", "cherry"]
+print(myfruitList)
+print(type(myfruitList))
+print(myfruitList[0])
+print(myfruitList[1])
+print(myfruitList[2])
+myfruitList[2] = "orange"
+print(myfruitList)
+myfinalanswertuple = ("apple", "banana", "pineapple")
+print(myfinalanswertuple)
+print(type(myfinalanswertuple))
+print(myfinalanswertuple[0])
+print(myfinalanswertuple[1])
+print(myfinalanswertuple[2])
+myfavoritefruitdictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myfavoritefruitdictionary)
+print(type(myfavoritefruitdictionary))
+print(myfavoritefruitdictionary["Akua"])
+print(myfavoritefruitdictionary["Saanvi"])
+print(myfavoritefruitdictionary["Paulo"])
